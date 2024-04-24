487288e15e878b6f35c7de59f58ff5c24c9b4225
finleyjg <snh22fpu@uea.ac.uk>
2024-04-24 18:01:41 +0100

    minor change

--- dashboard.php
+++ dashboard.php
@@ -1,21 +1,21 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>Dashboard</title>
-        <link rel="stylesheet" type="text/css" href="styles2.css">
-
-    </head>
-    <body>
-        <header>
-            <ul>
-                <li><a href="index.php">Home</a></li>
-                <li><a class = "active" href="dashboard.php">Dashboard</a></li>
-                <li><a href="profile.php">Profile</a></li>
-                <!--<li class = "logout"><a href ="#logout">Logout</a></li>-->
-            </ul>
-        </header>
-        <h1>DASHBOARD</h1>
-        <h2>Upcoming Deadlines:</h2>
-
-    </body>
+<head>
+    <title>Dashboard</title>
+    <link rel="stylesheet" type="text/css" href="styles2.css">
+</head>
+<body>
+    <header>
+        <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a class="active" href="dashboard.php">Dashboard</a></li>
+            <li><a href="profile.php">Profile</a></li>
+            <!--<li class="logout"><a href="#logout">Logout</a></li>-->
+        </ul>
+    </header>
+    <h1>DASHBOARD</h1>
+    <h2>Upcoming Deadlines:</h2>
+    <div class="section-gap"></div>
+    <h2>Past Deadlines:</h2>
+</body>
 </html>
