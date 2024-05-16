15ffb699c636b4e65ba805fb29b334a161f82352
finleyjg <snh22fpu@uea.ac.uk>
2024-05-16 23:10:19 +0100

    profile2

--- profile.php
+++ profile.php
@@ -14,24 +14,29 @@ require_once 'includes/login_view.inc.php';
         <script src="scripts.js"></script>
 
     </head>
-    <body>
+    <body class="profileBody">
         <header>
-        <nav class = "navbar">
-                <img src = "images\websiteTestLogo.png" alt = "Test logo">
+        <nav class = "navbarProfile">
             <ul class = "navlinks">
                 <li><a href="index.php">Home</a></li>
                 <li><a href="dashboard.php">Dashboard</a></li>
-                <li><a class = "active" href="profile.php">Profile</a></li>
+                <li><a class = "activeProfile" href="profile.php">Profile</a></li>
                 <li><a href="agenda.php">Agenda</a></li>
                 <li><a href="jsontest.php">Upload</a></li>
                 <!--<li class = "logout"><a href ="#logout">Logout</a></li>-->
             </ul>
             </nav>
         </header>
-        <h1>Profile</h1>
-        <h2>Sign Up or Login</h2> 
+        <!-- <h1 class = 'centreTitle'>Profile</h1> -->
+        <div class="imageCentre">
+                    <img src = "images\websiteTestLogo.png" alt = "Test logo">
+        </div>
+
+        <h2 class = "profileHeader">Sign Up or Login</h2> 
     <!--Step 1:Adding HTML-->
-    <button onclick="document.getElementById('id01').style.display='block'" style="width:auto;">Sign Up</button> 
+    <div class="profileButtonContainer">
+    <button class = "profileSignupButton" onclick="document.getElementById('id01').style.display='block'" style="width:auto;">Sign Up</button> 
+    </div>
   
     <div id="id01" class="modal"> 
         <form class="modal-content signup" action="includes\signup.inc.php" method="post"> 
@@ -52,7 +57,10 @@ require_once 'includes/login_view.inc.php';
         </form>
 
         </div>
-        <button onclick="document.getElementById('id02').style.display='block'" style="width:auto;">Login</button> 
+        <div class="profileButtonContainer">
+        <button class = "profileLoginButton" onclick="document.getElementById('id02').style.display='block'" style="width:auto;">Login</button> 
+        </div>
+        
   
     <div id="id02" class="modal"> 
         <form class="modal-content login" action="includes\login.inc.php"  method="post"> 
